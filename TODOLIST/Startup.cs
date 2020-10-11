dab7f8fd781e798339a1b3f872315a07f38a4464
Jose Guzman Martinez <j.alejandro.guzman@hotmail.com>
2020-10-11 17:01:44 -0400

    agregar archivos a github

---
+++ TODOLIST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TODOLIST.Startup))]
+namespace TODOLIST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
